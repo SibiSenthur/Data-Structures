69066e09c05f6c2d0e808c8485873f4b56afe2f0
Sibi Senthur <smuthu6@uic.edu>
2021-06-20 18:12:56 -0500

    Time: 140 ms (70.91%), Space: 15.5 MB (36.03%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -2,11 +2,11 @@ class Solution:
     
     def dfs(self, grid, r, c):
         grid[r][c] = '0'
-        lst = [(r-1,c),(r,c+1), (r, c-1), (r+1,c)]
+        lst = [(r-1,c), (r+1,c), (r,c-1), (r,c+1)]
         for row, col in lst:
             if row>=0 and col>=0 and row<len(grid) and col<len(grid[row]) and grid[row][col] == '1':
                 self.dfs(grid, row, col)   
-        
+                
     def numIslands(self, grid: List[List[str]]) -> int:
         islands = 0
         for r in range(len(grid)):
@@ -14,6 +14,4 @@ class Solution:
                 if grid[r][c] == '1':
                     self.dfs(grid, r, c)
                     islands +=1
-        return islands
-                    
-                
+        return islands             
